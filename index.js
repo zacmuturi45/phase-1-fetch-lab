038dd4a84d6e7290a88dd9c11b090c600d298635
isaacgichuhi <zacmuturi45@gmail.com>
2023-10-05 12:51:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ 
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON response
+  })
+  .then((books) => {
+    // Once the data is fetched and parsed, call the renderBooks function
+    renderBooks(books);
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
 }
 
 function renderBooks(books) {
